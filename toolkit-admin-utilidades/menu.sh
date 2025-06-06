b8314a235973bdb1afa6a5ae0a19712cacefa829
AAMinoldo <minoldoaldo@gmail.com>
2025-06-06 15:44:30 -0300

    build: cambio en nombre de carpeta en concordancia a maquina virtual linux

--- toolkit-admin-utilidades/menu.sh
+++ toolkit-admin-utilidades/menu.sh
@@ -8,12 +8,20 @@ while true; do
     echo "  TOOLKIT DE ADMINISTRACIÓN JR"
     echo "=================================="
     echo "1. Monitoreo y gestión de procesos"
+    echo "2. Informacion de memoria"
+    echo "3. Informacion de disco"
     echo "0. Salir"
     echo "=================================="
     read -p "Selecciona una opción: " opcion
 
     case $opcion in
-    1) bash procesos/procesos.sh ;;
+    1) bash procesos/procesos.sh
+       ;;
+    2) bash memoria/memoria.sh
+       ;;
+    3) bash disco/disco.sh
+       ;;
+       
     0)
         echo "Saliendo..."
         break
