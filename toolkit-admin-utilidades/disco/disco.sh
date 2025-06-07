c8249c9487cd74fcb6a2da015ef76eed9bfb94fa
Julieta Cabrera <juliecabrera56@gmail.com>
2025-06-07 19:26:31 -0300

    Func. Estadistica de vmstat

--- toolkit-admin-utilidades/disco/disco.sh
+++ toolkit-admin-utilidades/disco/disco.sh
@@ -36,3 +36,11 @@ else
 fi
 
 read -p "Presiona Enter para volver al menu..."
+
+echo
+echo "================Estadisticas de vmstat=============================="
+#vmstat muestra estadisticas- -s resumen(lista)
+vmstat -s | head -20
+
+echo
+read -p "Presiona Enter para volvel al menu...."
