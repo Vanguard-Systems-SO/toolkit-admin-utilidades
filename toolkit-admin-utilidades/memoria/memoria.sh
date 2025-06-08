79b9b8c9322b624527f430377d4b5c9dfcb77580
JuliCabrera <juliecabrera56@gmail.com>
2025-06-08 11:18:09 -0300

    Función para vmstat

--- toolkit-admin-utilidades/memoria/memoria.sh
+++ toolkit-admin-utilidades/memoria/memoria.sh
@@ -1,2 +1,9 @@
 #Aqui va lo de memoria
 
+echo
+echo "================Estadisticas de vmstat=============================="
+#vmstat muestra estadisticas- -s resumen(lista)
+vmstat -s | head -20
+
+echo
+read -p "Presiona Enter para volvel al menu...."
