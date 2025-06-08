cf0d2ceca55eeec37e8ac367bc19490b486d2006
Patricio Esteban Rodriguez <108308481+1PatoRod@users.noreply.github.com>
2025-06-08 11:19:29 -0300

    Memoria RAM y SWAP

--- toolkit-admin-utilidades/memoria/memoria.sh
+++ toolkit-admin-utilidades/memoria/memoria.sh
@@ -1,4 +1,17 @@
 #Aqui va lo de memoria
+#!/bin/bash
+
+clear
+echo "================Informacion de Memoria RAM y SWAP===================="
+#muestra el uso de la memoria ram mb o gb en uso o libre
+free -h
+
+echo
+echo "================Informacion adicional(/proc/meminfo)================="
+#muestra detalles de la memoria- proc/meminfo es un archivo virtual
+#grep busca texto -E para expresiones regulares
+grep -E 'MemTotal|Buffers|Cached|SwapTotal|SwapFree' /proc/meminfo
+
 
 echo
 echo "================Estadisticas de vmstat=============================="
